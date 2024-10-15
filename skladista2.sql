6f088816e81c2412cf3e5995176c2eb7b74dee96
filipilic01 <filipilicbarsa@gmail.com>
2024-10-15 15:02:46 +0200

    main

---
+++ skladista2.sql
@@ -0,0 +1,17 @@
+select * from Club 
+select * from Stadium
+select * from Competition
+select * from player  
+SELECT * FROM GAME 
+SELECT * FROM match_date
+select * from fact 
+
+
+/*
+DROP TABLE MATCH_DATE
+drop table Game
+drop table Competition
+drop table Stadium
+DROP table fact
+drop table Club
+drop table player*/
